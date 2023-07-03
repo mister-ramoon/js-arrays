e8378ce38db70f10020054608ab4ed8645ef0484
ramonruiz <jruiz@lightia.mx>
2023-07-03 17:38:28 -0600

    Join

---
+++ server/11-join-split.js
@@ -0,0 +1,7 @@
+const elements = ['Fire', 'Air', 'Water'];
+
+const rta = elements.join('--');
+console.log('rta', rta);
+
+const title = 'Curso de manipulación de arrays';
+const urlFinal = title.split(' ').join('-').toLowerCase();
